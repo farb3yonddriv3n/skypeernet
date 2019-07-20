c9143702b2322cbd75a7277c175e9432f35ecefb
farb <far.b3yond.driv3n@gmail.com>
2019-07-20 21:38:26 +0200

    Skynet package spec added

---
+++ plugins/skynet/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+     name='skypeernet',
+     version='0.1',
+     author="farb",
+     author_email="far.b3yond.driv3n@gmail.com",
+     description="skypeernet p2p network - skynet plugin",
+     url="https://github.com/farb3yonddriv3n/skypeernet",
+     packages=find_packages(),
+     scripts=['skypeernet.py'],
+ )
