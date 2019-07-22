7d86c0dfeb54b5a165486ab8f601e82b0414843a
farb <far.b3yond.driv3n@gmail.com>
2019-07-22 22:22:26 +0200

    Version format modified

--- tools/version/main.sh
+++ tools/version/main.sh
@@ -1,5 +1,8 @@
+major=0
+minor=1
 commits=`git rev-list HEAD --count`
 echo "#ifndef SPN_VERISON_H_
 #define SPN_VERSION_H_
-#define SPN_VERSION 0.$commits
+#define SPN_VERSION $major.$minor.$commits
 #endif" > src/include/spn_version.h
+echo "$major.$minor.$commits" > VERSION
