a2b20430f3fa7871a925ae1a37b000a434ea9cf0
farb <far.b3yond.driv3n@gmail.com>
2019-08-17 22:19:17 +0200

    Plugin johnconnor: pipe read modified

--- plugins/johnconnor/src/pipe.erl
+++ plugins/johnconnor/src/pipe.erl
@@ -11,14 +11,7 @@ dispatch(Data) ->
 loop(Fifo, 0, []) ->
     receive
         {Fifo, {data, Data}} ->
-            {ListSize, Rest} = lists:split(4, Data),
-            <<Size:32>> = list_to_binary(lists:reverse(ListSize)),
-            if Size == length(Rest) ->
-                dispatch(Rest),
-                loop(Fifo, 0, []);
-            true ->
-                loop(Fifo, Size, Rest)
-            end;
+            msgstart(Fifo, Data);
         {_Pid, eof} ->
             read();
         _Unsupported ->
@@ -28,16 +21,31 @@ loop(Fifo, Size, Rest) ->
     receive
         {Fifo, {data, Data}} ->
             NewData = lists:append(Rest, Data),
-            if length(NewData) == Size ->
-                dispatch(NewData),
-                loop(Fifo, 0, []);
-            true ->
-                loop(Fifo, Size, NewData)
-            end;
+            msgready(Fifo, NewData, Size);
         {_Pid, eof} ->
             read();
         _Unsupported ->
             loop(Fifo, 0, [])
+    end.
+
+msgready(Fifo, NewData, Size) when length(NewData) > Size ->
+    {Arrived, ToProcess} = lists:split(Size, NewData),
+    dispatch(Arrived),
+    msgstart(Fifo, ToProcess);
+msgready(Fifo, NewData, Size) when length(NewData) == Size ->
+    dispatch(NewData),
+    loop(Fifo, 0, []);
+msgready(Fifo, NewData, Size) ->
+    loop(Fifo, Size, NewData).
+
+msgstart(Fifo, Data) ->
+    {ListSize, Rest} = lists:split(4, Data),
+    <<Size:32>> = list_to_binary(lists:reverse(ListSize)),
+    if Size == length(Rest) ->
+        dispatch(Rest),
+        loop(Fifo, 0, []);
+    true ->
+        loop(Fifo, Size, Rest)
     end.
 
 write(Json) ->
